ea03670c3c0475395398e9d17105133ab9fca3f0
Aan Aldy <aandaldi@gmail.com>
2018-12-26 21:37:43 +0700

    knn pertama dan kedua

--- KNN/testknn1.py
+++ KNN/testknn1.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("tes yah`")
+import matplotlib.pyplot as plt
+
+x=[7,7,3,1]
+y=[7,4,4,4]
+
+plt.scatter(x,y)
+
+#data baru
+plt.scatter(3,7)   #scatter untuk memasukkan data baru
+
+plt.xlabel("Daya")
+plt.ylabel("Kekuatan")
+
+plt.show()
